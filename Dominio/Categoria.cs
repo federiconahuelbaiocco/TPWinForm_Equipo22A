f3cfa6c7e8fc838999593d75e02504b572e530fb
fedelomas <fedekpo27012012@gmail.com>
2025-09-15 12:34:22 -0300

    agregado de las capas dominio y negocio

--- Dominio/Categoria.cs
+++ Dominio/Categoria.cs
@@ -9,7 +9,7 @@ namespace Dominio
 	public class Categoria
 	{
 		public int Id { get; set; }
-		public string Descripcion { get; set; }
+		public string Descripcion { get; set; }//
 
 		public override string ToString()
 		{
